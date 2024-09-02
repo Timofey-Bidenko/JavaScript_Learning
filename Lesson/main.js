70c7178dfe1608db05d0671ad8f11c2cfdf87985
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2024-09-02 18:54:56 +0200

    Test Connection with GitHub

---
+++ Lesson/main.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+// ??
+
+// a ?? b
+
+// console.log(0 ?? 10)
+// console.log("hello" ?? 10)
+// console.log(false ?? 10)
+// console.log("" ?? 10)
+// console.log(null ?? 10)
+// console.log(undefined ?? 10)
+
+// let number = 5 + 6 ** 3 / (3 * 8)
+
+// condition ? value1 : value 2
+// condition ? value1 : condition ? value2 : value 3
+// let age = 25
+// let message1 = age > 18 ? "Welcome" : "Good bye!";
+// let message2 = age > 18 ? "Welcome" : age < 0 ? "який дивний вік!" : "Good bye!"
+
+// console.log(message2)
+
