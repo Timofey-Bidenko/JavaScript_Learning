b55f048e47add8846c087e1602f49c32beec1cf7
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2024-09-02 22:08:07 +0200

    Homeworrk #3 completed!

--- Lesson/main.js
+++ Lesson/main.js
@@ -29,3 +29,151 @@
 
 // console.log(message2)
 
+
+// let a;
+// console.log("a ~>", a)
+// a = 3
+// console.log("a ~>", a)
+
+// let b = a
+// a = 5
+
+// console.log(a)
+// console.log(b)
+
+/* 
+Error conditions
+
+let a = 2
+let a = 3
+can't create multiple variables with same name
+
+console.log("a ~>", a)
+let a = 2
+can't print whats not created before
+
+const a = 3
+a = 5
+can't change constant values
+*/
+
+// const a = 3;
+// console.log(a)
+
+// _ $ (can be used in variable names)
+// let const typeof class function (reserved words, can't be used as a variable name)
+
+// console.log(user-first-name); >>> Not Allowed
+// console.log($user); >>> allowed
+// console.log(Привіт); >>> allowed
+// let Привіт = "Hello"; >>> allowed
+// console.log(_); >>> allowed, important
+// console.log($); >>> allowed, important
+
+// Англійська мова
+// Чутливість до регістру
+// let name = "John"
+// let Name = "Jane"
+// name and Name are different
+
+// camelCase >>> firstUserName 
+// snake_case >>> first_user_name
+// JS users use camelCase, it is just historically like so.
+
+// userId userName
+// is has can
+// isUserAutorized
+// canUserSignIn
+
+// switch
+
+let numOfMonths = 5
+
+/* 
+switch (numOfMonths) {
+    case 1:
+        console.log("Зима")
+        break;
+    
+    case 2:
+        console.log("Зима")
+        break;
+
+    case 3:
+        console.log("Весна")
+        break;
+    
+    case 4:
+        console.log("Весна")
+        break;
+
+    case 5:
+        console.log("Весна")
+        break;
+
+    case 6:
+        console.log("Літо")
+        break;
+
+    case 7:
+        console.log("Літо")
+        break;
+
+    case 8:
+        console.log("Літо")
+        break;
+
+    case 9:
+        console.log("Осінь")
+        break;
+
+    case 10:
+        console.log("Осінь")
+        break;
+
+    case 11:
+        console.log("Осінь")
+        break;
+
+    case 12:
+        console.log("Зима")
+        break;
+
+    default:
+        break;
+}
+*/
+
+switch (numOfMonths) {
+    case 1:
+    case 2:
+    case 12:
+        console.log("Зима")
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Весна")
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Літо")
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Осінь")
+        break;
+    default:
+        break;
+}
+
+
+// try catch
+
+try {
+    // code
+} catch (error) {
+    // error code
+}
