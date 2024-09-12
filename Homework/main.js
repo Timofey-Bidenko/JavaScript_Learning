f71379e3a09e79399653fe70d7c386c17adacddd
Timofey-Bidenko <timofeytimtimofey@gmail.com>
2024-09-12 20:42:45 +0200

    Added little changes to homework, removed a feature

--- Homework/main.js
+++ Homework/main.js
@@ -1,7 +1,7 @@
 function doubleLetter(str) {
     let newStr = ""
     for (let i = 0; i < str.length; i += 1) {
-        newStr += str[i] + str[i]
+        newStr += str[i].repeat(2)
     }
     return newStr
 }
@@ -24,17 +24,24 @@ function padString(str, length, symbol, toLeft) {
 }
 
 function camelCase(str, separator) {
+    console.log(str)
     const words = str.split(separator)
+    let i = 1
     let newStr = words[0].toLowerCase()
-    for (let i = 1; i < words.length; i += 1) {
+    if (str.startsWith(separator)) {
+        newStr = words[1].toLowerCase()
+        i = 2
+    }
+    
+    for (i; i < words.length; i += 1) {
         newStr += words[i][0].toUpperCase() + words[i].slice(1).toLowerCase()
     }
     return newStr
 }
 
-// console.log(doubleLetter("Nicer"))
+console.log(doubleLetter("Nicer"))
 
 // console.log(padString("Ivan", 6, "*"))
 
 // console.log(camelCase("this_Variable_Will_Become_camel_Case", "_"))
-// console.log(camelCase("_this_Variable_Will_Become_Pascal_Case", "_")) // Не баг, а фіча
+// console.log(camelCase("_this_Variable_Will_NO_MORE_Become_Pascal_Case", "_")) // Не баг, а фіча [ФІЧУ ПРИБРАНО]
